69761dbe8808f840a87ba38d3b91899b3bbff945
alexey.blinyaev <oktimus.a@gmail.com>
2017-11-29 10:24:34 +0200

    test!

--- My_lesson/item-1/1.1.py
+++ My_lesson/item-1/1.1.py
@@ -2,10 +2,23 @@ def variant1(s):
     text = s[::-1]
     return text
 
+def variant3(s):
+    s1=list(s)
+    s1.reverse()
+    text=''.join(s1)
+    return text
 
+def variant4():
 
+    return text
 
 s = "qwertyuiop"
 print variant1(s)
+
+
+print variant3(s)
+
+print variant4(s)
+
 
 
