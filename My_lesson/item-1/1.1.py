2cd0e6f0f676dcbe0bd2d36f16152af8576565f8
Alex <oktimus.a@gmail.com>
2017-11-29 21:07:37 +0200

    1

--- My_lesson/item-1/1.1.py
+++ My_lesson/item-1/1.1.py
@@ -2,23 +2,32 @@ def variant1(s):
     text = s[::-1]
     return text
 
-def variant3(s):
+def variant2(s):
     s1=list(s)
     s1.reverse()
     text=''.join(s1)
     return text
 
-def variant4():
+def variant3(s):
+    l=[]
+    for i in s:
+        l.insert(0,i)
+    text=''.join(l)
+    return text
+
+
+def variant4(s):
+    l=list(s)
+    text=''.join(reversed(l))
 
     return text
 
-s = "qwertyuiop"
+
+
+s = "ambulance"
 print variant1(s)
-
-
+print variant2(s)
 print variant3(s)
-
 print variant4(s)
-
 
 
