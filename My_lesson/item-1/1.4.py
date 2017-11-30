6f2417cd491b1de9c72840a4b5f85d0c7e45676a
alexey.blinyaev <oktimus.a@gmail.com>
2017-11-30 13:43:01 +0200

    test!

--- My_lesson/item-1/1.4.py
+++ My_lesson/item-1/1.4.py
@@ -1,21 +1,21 @@
 def poisk(s):
-    s1=[]
-    s2=[]
-    end=''
-    for i in range(len(s)-1):
+    s1 = []
+    s2 = []
+    end = ''
+    for i in range(len(s) - 1):
         s1.append(s[i])
 
-        if ord(s[i])>ord(s[i+1]):
-                    s2.append(''.join(s1))
-                    s1=[]
+        if ord(s[i]) > ord(s[i + 1]):
+            s2.append(''.join(s1))
+            s1 = []
     print s2
     for i1 in s2:
-        if len(i1)>len(end):
-            end=i1
+        if len(i1) > len(end):
+            end = i1
+
     return (end)
 
 
-
-s = "sabrrtuwacaddabraabcdefghtuw"
+s = "sabrrtuwacaddabraabcdefghtuw "
 
 print poisk(s)
